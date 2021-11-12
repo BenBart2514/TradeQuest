f036b73309dc30cf34943877f5609684e7bfbfcb
BenBart2514 <b.bartholomew2514@gmail.com>
2021-11-12 12:50:57 +1000

    added header as partial to appear before notices and alerts on all pages

--- app/controllers/marketplace_controller.rb
+++ app/controllers/marketplace_controller.rb
@@ -3,5 +3,8 @@ class MarketplaceController < ApplicationController
     if current_user.hero == nil
       redirect_to new_hero_path
     end
+    @page_name = "Marketplace"
+    @top_link = "Go Questing"
+    @link_path = questing_path
   end
 end
--- app/controllers/questing_controller.rb
+++ app/controllers/questing_controller.rb
@@ -1,4 +1,7 @@
 class QuestingController < ApplicationController
     def index
+        @page_name = "Questing"
+        @top_link = "Go to Marketplace"
+        @link_path = root_path
     end
 end
