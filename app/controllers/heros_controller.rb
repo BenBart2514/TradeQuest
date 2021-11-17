fd26571223b354a369851c082628acc24c8a1258
BenBart2514 <b.bartholomew2514@gmail.com>
2021-11-17 19:32:38 +1000

    minor hero fix

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -19,13 +19,13 @@ class HerosController < ApplicationController
     begin
       @hero.save!
       Equipment.create(hero_id: @hero.id)
-      Weapon.create(hero_id: @hero.id, name: 'Old Sword', quality: Quality.find(1), type: Type.find(5), uses: 0)
+      first = Weapon.create(hero_id: @hero.id, name: 'Old Sword', quality: Quality.find(1), type: Type.find(5), uses: 0)
       first.image.attach(io: File.open('app/assets/images/Sword.png'),
                          filename: 'Sword.png', content_type: 'image/png')
       redirect_to root_path
     rescue StandardError
       flash[:alert] = @hero.errors.full_messages.flatten.join(', ')
-      render 'new'
+      redirect_to root_path
     end
   end
 
